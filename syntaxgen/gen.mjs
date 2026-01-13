5d78c271d8489a2bd3d18c89f03ff49322363c5f
Nicholas Shanks <nicholas.shanks@bbc.co.uk>
2026-01-13 08:35:29 +0000

    Move top-level keywords into a text file.

--- syntaxgen/gen.mjs
+++ syntaxgen/gen.mjs
@@ -3,35 +3,35 @@ import path, { dirname } from "path";
 import process from "process";
 import { fileURLToPath } from "url";
 
-function indentedPatternFromFile(filename) {
+const repositoryItem = (filename, prefix) => {
   const keywords = fs
     .readFileSync(filename, "utf8")
     .split("\n")
-    .filter((x) => !x.startsWith("#"))
-    .filter((x) => x); // empty string is falsy
+    .filter((x) => x && !x.startsWith("#"));
 
   const name = path.basename(filename, ".txt");
 
-  const multiWord = keywords.filter((w) => w.includes(" "));
-  const singleWord = keywords.filter((w) => !w.includes(" "));
+  const multiWord = keywords.filter((w) => w.includes(" ")).join("|");
+  const singleWord = keywords.filter((w) => !w.includes(" ")).join("|");
 
-  const pat = `"(?<=\\\\t| )((\\"(${multiWord.join(
-    "|"
-  )})\\")|((?<optionalquote>\\"?)(${singleWord.join(
-    "|"
-  )})\\\\b\\\\k<optionalquote>))"`;
+  const pat = `"${prefix}((\\"(${multiWord})\\")|((?<optionalquote>\\"?)(${singleWord})\\\\b\\\\k<optionalquote>))"`;
 
   return `
-		"${name}Generated": {
-			"patterns": [{
-				"name": "keyword.control.endlesssky",
-				"match": ${pat} 
-			}]
+		"${name}": {
+			"patterns": [
+				{
+					"name": "keyword.control.endlesssky",
+					"match": ${pat}
+				}
+			]
 		},`;
-}
+};
 
-function language() {
-  const files = ["keywords.txt"];
+const language = () => {
+  const files = [
+    ["indentedKeywords.txt", `(?<=\\\\t|  )`],
+    ["topLevelKeywords.txt", `^`],
+  ];
   const __dirname = dirname(fileURLToPath(import.meta.url));
   const template = fs.readFileSync(
     __dirname + "/template.tmLanguage.json",
@@ -43,12 +43,14 @@ function language() {
   if (insertionPoint === -1) {
     throw new Error("bad repository header or template");
   }
-  const insert = files
-    .map((f) => indentedPatternFromFile(__dirname + "/" + f))
-    .join("");
-  const output =
-    template.slice(0, insertionPoint) + insert + template.slice(insertionPoint);
-  return output;
-}
+
+  return (
+    template.slice(0, insertionPoint) +
+    files
+      .map(([f, prefix]) => repositoryItem(__dirname + "/" + f, prefix))
+      .join("") +
+    template.slice(insertionPoint)
+  );
+};
 
 process.stdout.write(language());
